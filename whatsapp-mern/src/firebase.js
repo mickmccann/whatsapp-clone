1a133f973b7668bd09aa8bc7347908d0a181ed0c
mickmccann <mickmccann@ymail.com>
2022-05-20 15:41:22 +0000

    created firebase.js and copied config settings from firebase website.

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHbDTP4qzQAp43gFCWP4ZdscJ3cMnhaIo",
+    authDomain: "whatsapp-mern-c5170.firebaseapp.com",
+    projectId: "whatsapp-mern-c5170",
+    storageBucket: "whatsapp-mern-c5170.appspot.com",
+    messagingSenderId: "663008572999",
+    appId: "1:663008572999:web:7e996b3ae522077b5aa362"
+  };
