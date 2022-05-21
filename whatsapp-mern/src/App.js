fc5e19326977e9cfdf8d72154c0a84e6b1f04955
mickmccann <mickmccann@ymail.com>
2022-05-21 16:21:22 +0000

    Created the sidebar component.

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <h1>WhatsApp MERN Clone</h1>
+      
       {/* Sidebar */}
+      <Sidebar  />
+
       {/* Chat component */}
     </div>
   );
--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';  
+import './Sidebar.css';
 
 function sidebar() {
   return (
     <div className='sidebar'>
-
+      <h1>Sidebar component goes here.</h1>
     </div>
   )
 }
