ef710d380fdb6f45f7e49336b91e3da039de7f28
mickmccann <mickmccann@ymail.com>
2022-06-03 15:49:17 +0000

    Created the chat__body and chat__message and gave the the stlying.

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -26,6 +26,13 @@ function Chat() {
             </IconButton>
           </div>
         </div>
+        <div className='chat__body'>
+          <p className='chat__message'>
+            <span className='chat__name'>Michael</span>
+            Hello world!
+            <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+          </p>
+        </div>
     </div>
   )
 }
